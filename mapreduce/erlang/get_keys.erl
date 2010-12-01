b55549024844ff54f6b64e60ab41602427ed6a48
Grant Schofield <grant@surlysoft.com>
2010-12-01 17:02:53 -0600

    adding get_keys/delete_keys

---
+++ mapreduce/erlang/get_keys.erl
@@ -0,0 +1,27 @@
+%% -------------------------------------------------------------------
+%%
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%  http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
+
+-module(map_functions).
+
+-export([get_keys/3]).
+
+%Returns bucket and key pairs from a map phase
+get_keys(Value,_Keydata,_Arg) ->
+  [{riak_object:bucket(Value),riak_object:key(Value)}].
